e9614c6bf909906d66e0b6479b1183548cd59011
HarryP7 <grishkamartov@gmail.com>
2020-01-03 16:09:35 +0300

    Added

---
+++ WebApiRH/Models/ViewModel/AddAddressModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApiRH.Models.ViewModel
+{
+    public class AddAdressModel
+    {
+        public String Fk_User { get; set; }
+        public String Fk_Home { get; set; }
+        public int Appartment { get; set; }
+        public String Address { get; set; }        
+    }
+}
